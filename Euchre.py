9f761c9af441f94883657d3116992b3e72ce75ad
Blake Pozolo <btpozolo@umich.edu>
2024-05-24 13:36:39 -0700

    updated display for game

--- Euchre.py
+++ Euchre.py
@@ -16,6 +16,9 @@ PAIRED_SUITS = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
 NUMBERS = ['Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
 POWER = [1, 2, 3, 4, 5, 6]
 
+GREEN = "\033[32m"
+RESET = "\033[0m"
+
 def create_deck():
     cards = {
         #('King', 'Hearts'): {'effective_suit': 'Hearts', 'suit_power': 2, 'trump': False} 
@@ -104,19 +107,44 @@ def deal_cards(cards):
 
     return (hands[0], hands[1], hands[2], hands[3], showing_card)
 
+def display_cards(cards, show_num=True):
+    '''
+    Need to be passed a list of card objects then displays them with number for playing.
+    '''
+    # Construct the card string
+    num_cards = len(cards)
+
+    print('+----------+ '* num_cards)
+    if show_num:
+        print("".join([f'|{GREEN}{num}{RESET}         | ' for num in range(1, num_cards + 1)]))
+    else:
+        print('|          | ' * num_cards)
+    print("".join([f'|{card[0]:^10}| ' for card in cards]))
+    print('|    of    | ' * num_cards)
+    print("".join([f'|{card[1]:^10}| ' for card in cards]))
+    print('|          | ' * num_cards)
+    print('+----------+ ' * num_cards)
+
+    return
+
+
 def get_play(hand):
     return
 
 def play_hand(cards):
     # deal hands 
     P1, P2, P3, P4, showing_card = deal_cards(cards)
+    print('Players hand is:')
+    display_cards(P1)
+    print('Please select the number of a card to play:')
+    p1_play = int(input()) - 1
+    print(f'You played the {P1[p1_play][0]} of {P1[p1_play][1]}.')
+    # Validate legal play
 
-    # set trump
-    # TODO
     set_trump('Hearts', cards)
 
     # Play cards
-    card1 = ('King', 'Hearts')
+    card1 = P1[p1_play]
     card2 = ('Queen', 'Hearts')
     card3 = ('King', 'Spades')
     card4 = ('Jack', 'Diamonds')
@@ -129,11 +157,85 @@ cards = create_deck()
 play_hand(cards)
 pp.pprint(cards)
 
+''''
+       User  Comp                               +---------+    
+ Score:  5      7         Partner               |  Queen  |
+Tricks:  2      2                               | Diamonds|
+                           Nine                 +---------+
 
+        User  Comp           D                    
+ Score:  5      7         Partner
+Tricks:  2      2  
+                           Nine
+ D                        Spades                        D
+Computer 1       King                    Queen        Computer 2
+                Hearts                  Diamonds
+                            Queen
+                            Clubs
+Player's hand D
++----------+ +----------+ +----------+ +----------+ +----------+ 
+|1         | |2         | |3         | |4         | |5         | 
+|   King   | |  Queen   | |  Queen   | |   Ten    | |   Ten    | 
+|    of    | |    of    | |    of    | |    of    | |    of    | 
+|  Hearts  | |  Hearts  | |  Clubs   | |  Clubs   | |  Spades  | 
+|          | |          | |          | |          | |          | 
++----------+ +----------+ +----------+ +----------+ +----------+ 
+'''
 
-set_trump('Hearts', cards)
-clear_trump(cards)
+''.endswith()
 
-pp.pprint(cards)
+def to_jaden_case(string):
+    return ' '.join([word.capitalize() for word in string.split()])
+ 
+print(to_jaden_case('HOW CAN MIRRORS BE REAL'))
 
+"HELLOW".capitalize()
 
+def persistence(num):
+    times = 0
+    while len(str(num)) > 1:
+        times += 1
+        number = 1
+        for char in str(num):
+            number *= int(char)
+        num = number
+    return times
+
+persistence(4)
+
+def longest(a1, a2):
+    chars = set(a1).union(a2)
+    chars = sorted(list(chars))
+    return ''.join(chars)
+
+longest('fdsalfdjsa', 'fdsafdsa')
+'''
+algo:
+    - remove before ://
+    - keep until '.'
+'''
+prefixes =[
+    'http://',
+    'https://',
+    'www.',
+]
+
+url = "https://hyphen-site.org"
+new_url = ''
+
+for pre in prefixes:
+    if url.startswith(pre):
+        l = len(pre)
+        i = url.find(pre)
+        url = url[i + l:]
+print(url)
+
+url = url.split('.')[0]
+
+print(url)
+
+if '://' in url:
+    _, _,  s = url.partition('://')
+    y, _, _ = s.partition('.')
+
+y
